d293086ab3a6ee69e19be9c33c33e58141452d47
Marco WAN <marcowan00@gmail.com>
2025-11-27 11:53:58 +0800

    Change gamehub logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,6 @@
-import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/react.svg";
+import { Box, HStack } from "@chakra-ui/react";
+// import logo from "../assets/react.svg";
+import { RiGameLine } from "react-icons/ri";
 import DarkModeSwitch from "./DarkModeSwitch";
 import SearchInput from "./SearchInput";
 
@@ -10,9 +11,14 @@ interface Props {
 const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize="60px" />
+      {/* <Image src={logo} boxSize="50px" marginX={3} /> */}
+      <Box marginX={3}>
+        <RiGameLine size="60px" />
+      </Box>
       <SearchInput onSearch={onSearch} />
-      <DarkModeSwitch />
+      <Box marginLeft={3}>
+        <DarkModeSwitch />
+      </Box>
     </HStack>
   );
 };
