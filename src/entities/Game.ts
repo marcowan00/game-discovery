4a77fd3dbef0fe9fe2abae18e1d4d3436f810d5c
Marco WAN <marcowan00@gmail.com>
2025-11-29 17:34:17 +0800

    Build game attributes component

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,12 +1,16 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 export interface Game {
   id: number;
   name: string;
   slug: string;
   background_image: string;
+  genres: Genre[];
   parent_platforms: { platform: Platform }[];
+  description_raw: string;
+  publishers: Publisher[];
   metacritic: number;
   rating_top: number;
-  description_raw: string;
 }
