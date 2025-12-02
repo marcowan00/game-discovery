f20f8ed9d81144d74a37cdea9a2c350d1a36a647
Marco WAN <marcowan00@gmail.com>
2025-12-02 19:22:43 +0800

    Handle empty favorites

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,7 +1,38 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import FavoriteList from "../components/favorites/FavoriteList";
+import useFavoritesStore from "../stores/favoritesStore";
 
 const FavoritesPage = () => {
-  return <FavoriteList />;
+  const favoritesCount = useFavoritesStore((s) => s.favorites.length);
+
+  if (favoritesCount === 0) {
+    return (
+      <Box textAlign="center" paddingY={20}>
+        <Heading size="lg" marginBottom={4}>
+          Nothing here.
+        </Heading>
+        <Link to="/">
+          <Text
+            fontSize="lg"
+            color="gray.500"
+            marginBottom={6}
+            textDecoration="underline"
+            transition="color 0.15s ease-in-out"
+            _hover={{ color: "gray.800" }}
+          >
+            Go find your favorite games!
+          </Text>
+        </Link>
+      </Box>
+    );
+  }
+
+  return (
+    <Box paddingY={10}>
+      <FavoriteList />;
+    </Box>
+  );
 };
 
 export default FavoritesPage;
